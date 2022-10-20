aad9f7f21178a2afa39d788ac34a3de335a2728c
Ivan Carlos <ivancarlos50@gmail.com>
2022-10-19 22:58:19 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/ivancarlos/workshopmongo/services/UserService.java
+++ src/main/java/com/ivancarlos/workshopmongo/services/UserService.java
@@ -31,6 +31,12 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+		findAll();
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
